feb97e09be028793bc531ce450d9a8125e177657
Dan Siwiec <daniel.siwiec@gmail.com>
2014-10-07 00:44:21 -0700

    Adding an empty DELETE

--- src/main/java/resources/TodoResource.java
+++ src/main/java/resources/TodoResource.java
@@ -2,10 +2,7 @@ package resources;
 
 import model.Todo;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -20,5 +17,9 @@ public class TodoResource {
     @POST
     public Todo addTodos(Todo todo) {
         return todo;
+    }
+
+    @DELETE
+    public void emptyDelete() {
     }
 }
