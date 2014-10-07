c77f3fedc2be9f8f414bf92ef412e09d16e14e5e
Dan Siwiec <daniel.siwiec@gmail.com>
2014-10-07 00:43:05 -0700

    Adding an empty GET

--- src/main/java/resources/TodoResource.java
+++ src/main/java/resources/TodoResource.java
@@ -2,6 +2,7 @@ package resources;
 
 import model.Todo;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +12,10 @@ import java.util.List;
 @Path("/todo")
 @Produces(MediaType.APPLICATION_JSON)
 public class TodoResource {
+
+    @GET
+    public void emptyGet() {
+    }
 
     @POST
     public Todo addTodos(Todo todo) {
