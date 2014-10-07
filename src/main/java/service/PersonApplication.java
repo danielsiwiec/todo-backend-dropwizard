996f1d95b7adee09280be3131ca2abccb766c6fe
Dan Siwiec <daniel.siwiec@gmail.com>
2014-10-07 00:33:50 -0700

    Adding CORS filter

--- src/main/java/service/PersonApplication.java
+++ src/main/java/service/PersonApplication.java
@@ -4,8 +4,13 @@ import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import resources.PersonResource;
 import resources.TodoResource;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class PersonApplication extends Application<Configuration> {
 
@@ -22,5 +27,16 @@ public class PersonApplication extends Application<Configuration> {
     public void run(Configuration configuration, Environment environment) throws Exception {
         environment.jersey().register(new PersonResource());
         environment.jersey().register(new TodoResource());
+
+        addCorsHeader(environment);
+    }
+
+    private void addCorsHeader(Environment environment) {
+        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter("allowedOrigins", "*");    // allowed origins comma separated
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin, Token");
+        filter.setInitParameter("allowedMethods", "GET,PUT,POST,DELETE,OPTIONS,HEAD");
+        filter.setInitParameter("allowCredentials", "true");
     }
 }
